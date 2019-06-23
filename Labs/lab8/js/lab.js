6aab69f752fc5a3daa2175fc643f1a5f76dabe0b
Vitya80100 <misterstop7@gmail.com>
2019-06-23 14:01:06 +0300

    add 8 lab ( don`t working )

---
+++ Labs/lab8/js/lab.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+  const body = document.body;
+  const container = this.document.querySelector('.input-container');
+  const input = new StrongConnectedComponent();
+  input.html(container);
+});
